cffadca3feea958245e457bae3f3d879a1e3926e
mahmoodahmad100 <mahmood073@gmail.com>
2018-06-13 15:19:43 +0200

    basic helper ... pre

--- vendor/helpers.php
+++ vendor/helpers.php
@@ -1,1 +1,17 @@
 <?php
+
+if(!function_exists('pre'))
+{
+	/**
+	 * pre function
+	 *
+	 * @var mixed $var
+	 * @return void
+	 */
+	function pre($var)
+	{
+		echo '<pre>';
+		print_r($var);
+		echo '</pre>';
+	}
+}
