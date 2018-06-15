01ae202df9792c3a9a6086075e494c4e2cc518dd
mahmoodahmad100 <mahmood073@gmail.com>
2018-06-15 11:40:15 +0200

    setting up

--- vendor/System/Session.php
+++ vendor/System/Session.php
@@ -35,4 +35,82 @@ class Session
 			session_start();
 	}
 
+	/**
+	 * create new session
+	 * 
+	 * @param string $key
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function set($key, $value)
+	{
+		$_SESSION[$key] = $value;
+	}
+
+	/**
+	 * get a session
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function get($key)
+	{
+		return $_SESSION[$key];
+	}
+
+	/**
+	 * checkout whether the session exists or not
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function has($key)
+	{
+		return isset($_SESSION[$key]);
+	}
+
+	/**
+	 * remove a session
+	 *
+	 * @param string $key
+	 * @return void
+	 */
+	public function remove($key)
+	{
+		unset($_SESSION[$key]);
+	}
+
+	/**
+	 * get the value from the session and  then unset the session
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function pull($key)
+	{
+		$value = $this->get($key);
+		$this->remove($key);
+		return $value;
+	}
+
+	/**
+	 * get all sessions
+	 *
+	 * @return array
+	 */
+	public function all()
+	{
+		return $_SESSION;;
+	}
+
+	/**
+	 * remove all sessions
+	 *
+	 * @return void
+	 */
+	public function destroy()
+	{
+		session_destroy();
+	}
+
 }
