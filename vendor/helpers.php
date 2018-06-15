2c883e5b51e70c0d234ba3570a53345f758c62f2
mahmoodahmad100 <mahmood073@gmail.com>
2018-06-15 11:53:48 +0200

    get_array helper

--- vendor/helpers.php
+++ vendor/helpers.php
@@ -15,3 +15,19 @@ if(!function_exists('pre'))
 		echo '</pre>';
 	}
 }
+
+if(!function_exists('get_array'))
+{
+	/**
+	 * get a value from the given array by providing the index($key) for this array
+	 *
+	 * @param array          $array
+	 * @param string|integer $key
+	 * @param mixed          $default
+	 * @return mixed
+	 */
+	get_array($array, $key, $default = null)
+	{
+		return isset($array[$key]) ? $array[$key] : $default;
+	}
+}
