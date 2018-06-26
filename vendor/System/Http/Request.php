b8d0e0ddf6dde0e88ae34175d6bb118e8e341ddf
mahmoodahmad100 <mahmood073@gmail.com>
2018-06-26 20:40:58 +0200

    init new methods

--- vendor/System/Http/Request.php
+++ vendor/System/Http/Request.php
@@ -39,4 +39,39 @@ class Request
 	{
 		
 	}
+
+	/**
+	 * get the value from $_GET
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function get($key)
+	{
+		return get_array($_GET, $key);
+	}
+
+	/**
+	 * get the value from $_POST
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function post($key)
+	{
+		return get_array($_POST, $key);
+	}
+
+	/**
+	 * get the value from $_SERVER
+	 *
+	 * @param string $key
+	 * @return mixed
+	 */
+	public function server($key)
+	{
+		return get_array($_SERVER, $key);
+	}
+
+
 }
